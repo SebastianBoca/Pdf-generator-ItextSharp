73c1362714842c4d89b21f2e47041b6edf6317a5
SebastianBoca <bocasebastian@outlook.com>
2018-01-27 20:22:54 -0800

    Pdf generator
    des

---
+++ pdf rendering/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_rendering
+{
+    public class Product
+    {
+        public string name { get; set; }
+
+        public string description { get; set; }
+    }
+
+    public class Products
+    {
+        public List<Product> GetProducts()
+        {
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product() { name = "Colgate", description = "Ships in stacks of 4." });
+            productList.Add(new Product() { name = "Aquafresh", description = "The packaging looks flimsy." });
+            productList.Add(new Product() { name = "Blend-a-med", description = "A lot of commercials." });
+            return productList;
+        }
+    }
+}
---
+++ pdf rendering/Program.cs
@@ -0,0 +1,97 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf_rendering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paragraph emptyRow = new Paragraph(" ");
+            int index = 0;
+
+            FileStream fs = new FileStream("Transaction.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+            doc.Open();
+
+            /// Title
+            Paragraph pr = new Paragraph("Nota de intrare");
+            pr.IndentationLeft = 250f;
+            doc.Add(pr);
+            doc.Add(emptyRow);
+
+            /// Operation parties
+            Paragraph initiatingAccount = new Paragraph("Initiating account:    Company one");
+            initiatingAccount.IndentationLeft = 50f;
+            doc.Add(initiatingAccount);
+
+            Paragraph toAccount = new Paragraph("To account:    Company two");
+            toAccount.IndentationLeft = 50f;
+            doc.Add(toAccount);
+
+            Paragraph typeOfTransaction = new Paragraph("Type of transaction:    Inventory");
+            typeOfTransaction.IndentationLeft = 50f;
+            doc.Add(typeOfTransaction);
+
+            doc.Add(emptyRow);
+
+            /// Product table
+            PdfPTable myTable = new PdfPTable(3);
+
+            PdfPCell cell;
+            cell = new PdfPCell(new Phrase("Name"));
+            cell.HorizontalAlignment = 1;
+            myTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase("Description"));
+            cell.HorizontalAlignment = 1;
+            myTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase("Quantity"));
+            cell.HorizontalAlignment = 1;
+            myTable.AddCell(cell);
+
+            List<Product> productList = new Products().GetProducts();
+
+            foreach(Product p in productList)
+            {
+                index++;
+                cell = new PdfPCell(new Phrase(p.name));
+                myTable.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(p.description));
+                myTable.AddCell(cell);
+
+                cell = new PdfPCell(new Phrase(index.ToString()));
+                cell.HorizontalAlignment = 1;
+                myTable.AddCell(cell); 
+            }
+
+            doc.Add(myTable);
+
+            doc.Add(emptyRow);
+
+            PdfPTable final = new PdfPTable(2);
+            cell = new PdfPCell(new Phrase(DateTime.Now.ToString()));
+            cell.BorderColor = Color.WHITE;
+            final.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase("Signature"));
+            cell.BorderColor = Color.WHITE;
+            cell.HorizontalAlignment = PdfPCell.ALIGN_RIGHT;
+            final.AddCell(cell);
+
+            
+            doc.Add(final);
+
+            doc.Close();
+        }
+    }
+}
